54fdc3e77fbef17cd2f9b7a3a3a21ddcb7ce4dce
w311ang <30381083+w311ang@users.noreply.github.com>
2021-07-25 15:37:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,5 +12,8 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
 sed -i "s/close_log_tcp '0'/close_log_tcp '1'/g" feeds/passwall/luci-app-passwall/root/etc/config/passwall
 sed -i "s/close_log_udp '0'/close_log_udp '1'/g" feeds/passwall/luci-app-passwall/root/etc/config/passwall
+cat modem_direct.txt >> package/network/config/firewall/files/firewall.user
+echo >> package/network/config/firewall/files/firewall.user
